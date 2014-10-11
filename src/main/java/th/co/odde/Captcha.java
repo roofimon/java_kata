454fac7f2b94e48bed3d0620d5029dec68fab770
Roof Panichsombat <roofimon@gmail.com>
2014-10-11 17:29:32 +0700

    Remove pattern from Captcha

--- src/main/java/th/co/odde/Captcha.java
+++ src/main/java/th/co/odde/Captcha.java
@@ -1,11 +1,8 @@
 package th.co.odde;
 
 public class Captcha {
-    private final int INT_PATTERN = 1;
-    private int pattern;
     private Operand left;
     public Captcha(int pattern, int leftOperand, int operator, int rightOperand){
-        this.pattern = pattern;
         this.left = Operand.createOperand(pattern, leftOperand);
     }
 
