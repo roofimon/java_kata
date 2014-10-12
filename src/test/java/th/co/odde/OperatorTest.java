055f118c638a8748d852a78648828e28ef7a8777
Roof Panichsombat <roofimon@gmail.com>
2014-10-12 21:20:37 +0700

    Add tests

--- src/test/java/th/co/odde/OperatorTest.java
+++ src/test/java/th/co/odde/OperatorTest.java
@@ -1,8 +1,21 @@
 package th.co.odde;
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 public class OperatorTest {
+    @Test
     public void ItShouldReturnPlusWhenInputIs1(){
         Operator operator = new Operator(1);
         assertEquals("+", operator.toString());
+    }
+    @Test
+    public void ItShouldReturnMinusWhenInputIs2(){
+        Operator operator = new Operator(2);
+        assertEquals("-", operator.toString());
+    }
+    @Test
+    public void ItShouldReturnMultiplyWhenInputIs3(){
+        Operator operator = new Operator(3);
+        assertEquals("*", operator.toString());
     }
 }
