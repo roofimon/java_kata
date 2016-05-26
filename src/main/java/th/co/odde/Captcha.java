02d8c6e856953ad9f0c479c0dadbd763e591a614
roofimon <roofimon@gmail.com>
2016-05-26 11:17:21 +0700

    Add Test LeftShouldBe1WhenPatternIs2AndLeftIs1

---
+++ src/main/java/th/co/odde/Captcha.java
@@ -0,0 +1,17 @@
+package th.co.odde;
+
+public class Captcha {
+    private int left;
+    private int pattern;
+    public Captcha(int pattern, int left, int operator, int right) {
+        this.left = left;
+        this.pattern = pattern;
+    }
+
+    public String getLeft() {
+        String[] intToString = {"ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"};
+        if (this.pattern == 2)
+            return "1";
+        return intToString[this.left-1];
+    }
+}
--- src/test/java/th/co/odde/CaptchaTest.java
+++ src/test/java/th/co/odde/CaptchaTest.java
@@ -5,8 +5,29 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CaptchaTest {
+
+    public static final int DUMMY_OPERATOR = 1;
+    public static final int DUMMY_RIGHT = 1;
+
     @Test
-    public void thisTestShouldFailed(){
-        assertEquals("9", "8");
+    public void leftOperandShouldBeONEWhenPatternIs1AndLeftIs1(){
+        Captcha captcha = new Captcha(1, 1, DUMMY_OPERATOR, DUMMY_RIGHT);
+        assertEquals("ONE", captcha.getLeft());
+    }
+    @Test
+    public void leftOperandShouldBeTWOWhenPatternIs1AndLeftIs6() {
+        Captcha captcha = new Captcha(1, 6, DUMMY_OPERATOR, DUMMY_RIGHT);
+        assertEquals("SIX", captcha.getLeft());
+    }
+    @Test
+    public void leftOperandShouldBeNINEWhenPatternIs1AndLeftIs9() {
+        Captcha captcha = new Captcha(1, 9, DUMMY_OPERATOR, DUMMY_RIGHT);
+        assertEquals("NINE", captcha.getLeft());
+    }
+
+    @Test
+    public void leftOperandShouldBe1WhenPatternIs2AndLeftIs1() {
+        Captcha captcha = new Captcha(2, 1, DUMMY_OPERATOR, DUMMY_RIGHT);
+        assertEquals("1", captcha.getLeft());
     }
 }
